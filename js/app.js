9b4a5b882e98d877f7ed135c09a9c1473af2e844
James Lock <james01@ataye.com.au>
2013-03-03 14:01:58 +1000

    Using AngularJS to drive data updates including popup panels.

--- js/app.js
+++ js/app.js
@@ -1,158 +1,198 @@
 
+var iUpdate = 250;
+var format = d3.format(",d");
 
-(function(io) {
+// main app:
+var appCtrl = function($scope, $compile){
+    $scope.twits = [];          // array of tweet lengths.
+    $scope.twitChange = [];     // changes prior to ui update
+    $scope.panels = [];         // array of open panels.
+    $scope.counter = 0;         // total number of tweets received.
 
-    var twitData = {children:[]};       // the main twitter data array
-    var twitChange = {children:[]};     // changes prior to ui update
-    var iTwitCount = 0;
-    var iUpdate = 250;
-    var format = d3.format(",d");
 
-    // setup the graph:
-    var graph = function () {
+    // update the tweets array:
+    var fnUpdate = function(len){
+        $scope.counter++;
 
-        // get window size:
-        var win = function(){
-            var db = document.body;
-            var dde = document.documentElement;
+        // find element and update, or insert:
+        $scope.$apply(function(){
+            var iName = len;
+            var o = _.find($scope.twits, function (itm) {
+                return itm.name == iName;
+            });
+            if (o)
+                o.value += 1;
+            else{
+                o = {name:iName, value:1};
+                $scope.twits.push(o);
+            }
 
-            return {
-                width: Math.max(db.scrollWidth, dde.scrollWidth, db.offsetWidth, dde.offsetWidth, db.clientWidth, dde.clientWidth) - 30
-                , height: Math.max(db.scrollHeight, dde.scrollHeight, db.offsetHeight, dde.offsetHeight, db.clientHeight, dde.clientHeight) - 80
-            };
-        }();
+            // is this a new change:
+            if (!_.find($scope.twitChange, function (itm) { return itm.name == iName; }))
+                $scope.twitChange.push(o);
 
+            // ok, call update:
+            graph.update();
+        });
+    };
+
+    // click events:
+    var fnItem_Click = function(dat){
+        // check if panel is already open:
+        if (_.contains($scope.panels, dat.name)){
+            // already showing, ignore!
+            return;
+        }
+        $.jGrowl("<span>{{dat.value}}</span>", {
+            sticky:true
+            , beforeOpen:function(e, m, o){
+                // create new scope and compile new template:
+                var $sc = $scope.$new();
+                $sc.item = dat;
+                $sc.rank = function(){
+                    // calculate the rank by ordering by value (descending) and get item index:
+                    var ar = _.sortBy($scope.twits, function(itm){ return itm.value * -1; });
+                    return ar.indexOf(dat) + 1; // fix for 0 based.
+                };
+
+                // insert hte template string and compile using new child scope:
+                e.append(angular.element('#pop-panel-tmpl').html());
+                $compile(e)($sc);
+
+                // add to panels array, so we only open 1 panel:
+                $scope.panels.push(dat.name);
+            }
+            , close:function(e, m, o){
+                // remove from panels array:
+                $scope.panels.splice($scope.panels.indexOf(dat.name), 1);
+            }
+        });
+    };
+
+    // define the d3js graph:
+    var graph = function(){
+        var win = helpers.win();
         var bubble = d3.layout.pack()
             .sort(null)
-            .size([win.width, win.height])//[diameter, diameter])
+            .size([win.width, win.height-90])
             .padding(6);
 
-        var svg = d3.select('svg') //.select("body").append("svg")
-            .attr("width", win.width)//diameter)
-            .attr("height", win.height-50)//diameter)
+        var svg = d3.select('svg')
+            .attr("width", win.width)
+            .attr("height", win.height-90)
             .attr("class", "bubble");
 
-        // graph update:
-        updateGraph = function () {
-            // get and clean the data:
-            var dataLocal = bubble.nodes(twitData)
-                .filter(function (d) {
-                    return !d.children;
+        // graph update (throttle to make sure we don't call it too often, reducing update performance):
+        update = _.throttle(function () {
+            return function(){
+                // get and clean the data:
+                var dataLocal = bubble.nodes({children:$scope.twits})
+                    .filter(function (d) {
+                        return !d.children;
+                    });
+
+                // calculate colours:
+                var colour = d3.scale.linear().domain([
+                    _.min(dataLocal,function (dat) {
+                        return dat.value;
+                    }).value
+                    , _.max(dataLocal,function (dat) {
+                        return dat.value;
+                    }).value
+                ]).range(['#506c77', '#ffffff']);
+
+                // calc graph:
+                var node = svg.selectAll('.node')
+                    .data(dataLocal, function (d) {
+                        return d.name;
+                    });
+
+                // new nodes, add a group to contain the circle and label:
+                var g = node.enter().append('g')
+                    .attr('class', 'node');
+
+                // add a circle to the group:
+                g.append('circle')
+                    .style('fill', '#ffffff')
+                    .on('mouseover', function(d){ d3.select(this).attr('stroke', '#dbf5ff'); })
+                    .on('mouseout', function(d){ d3.select(this).attr('stroke', 'none'); })
+                    .on('click', fnItem_Click);
+
+                // add the text label:
+                g.append('text')
+                    .attr("dy", ".3em")
+                    .attr('text-anchor', 'middle')
+                    .attr('pointer-events', 'none')
+                    .attr('pointer', 'none')
+                    .text(function (d) {
+                        return d.name;
+                    });
+
+                // now position the new group:
+                g.attr('transform', function (d) {
+                    return "translate(" + d.x + ',' + d.y + ")";
                 });
 
-            // calc colours:
-            var colour = d3.scale.linear().domain([
-                _.min(dataLocal,function (dat) {
-                    return dat.value;
-                }).value
-                , _.max(dataLocal,function (dat) {
-                    return dat.value;
-                }).value
-            ]).range(['#506c77', '#ffffff']);
+                // update all existing nodes (node doesn't yet contain new nodes):
+                node
+                    .transition().duration(iUpdate*1.2)
+                    .attr('transform', function (d) {
+                        return "translate(" + d.x + ',' + d.y + ")";
+                    });
 
-            // calc graph:
-            var node = svg.selectAll('.node')
-                .data(dataLocal, function (d) {
-                    return d.name;
-                })
-            ;
+                // now select circles within each group and set the radius and style:
+                node.select('circle')
+                    //.style('fill', '#fff')
+                    .transition().ease('elastic').duration(iUpdate*4)
+                    .attr('r', function (d) { return d.r; })
+                    .style('fill', function (d) { return colour(d.value); });
 
-            // new nodes, add a group to contain the circle and label:
-            var g = node.enter().append('g')
-                .attr('class', 'node')
-            ;
-            // add a circle to the group:
-            g.append('circle')
-                .style('fill', '#ffffff')
-                .on('mouseover', function(d){ d3.select(this).attr('stroke', '#dbf5ff'); })
-                .on('mouseout', function(d){ d3.select(this).attr('stroke', 'none'); })
-                .on('click', function(d){
-                    //$.jGrowl(d.name);
-                    console.log(d.name);
-                })
-            ;
-            // add the text label:
-            g.append('text')
-                .attr("dy", ".3em")
-                .attr('text-anchor', 'middle')
-                .attr('pointer-events', 'none')
-                .attr('pointer', 'none')
-                .text(function (d) {
-                    return d.name;
-                })
-            ;
-            // now position the new group:
-            g.attr('transform', function (d) {
-                return "translate(" + d.x + ',' + d.y + ")";
-            })
-            ;
+                // select new nodes to highlight:
+                var nodesnew = d3.selectAll('circle')
+                    .data(
+                    bubble.nodes({
+                        children:$scope.twitChange})
+                            .filter(function (d) { return !d.children; })
+                    , function (d) { return d.name; });
 
-            // update all existing nodes (node doesn't yet contain new nodes):
-            node
-                .transition().duration(iUpdate*1.2)
-                .attr('transform', function (d) {
-                    return "translate(" + d.x + ',' + d.y + ")";
-                })
-            ;
+                nodesnew
+                    .style('fill', '#ff7000')
+                    .transition().duration(100)
+                    .style('fill', '#fff')
+                    .transition().ease('elastic').duration(iUpdate*4)
+                    .style('fill', function (d) { return colour(d.value); });
 
-            // now select circles within each group and set the radius and style:
-            node.select('circle')
-                //.style('fill', '#fff')
-                .transition().ease('elastic').duration(iUpdate*4)
-                .attr('r', function (d) { return d.r; })
-                .style('fill', function (d) { return colour(d.value); })
-            ;
+                // clear new nodes:
+                $scope.twitChange = [];
 
-            // select new nodes to highlight:
-            var nodesnew = d3.selectAll('circle')
-                .data(
-                    bubble.nodes(twitChange)
-                    .filter(function (d) { return !d.children; })
-                , function (d) { return d.name; });
+                // return instance:
+                return this;
+            }
+        }, iUpdate)();
 
-            nodesnew
-                .style('fill', '#ff7000')
-                .transition().duration(100)
-                .style('fill', '#fff')
-                .transition().ease('elastic').duration(iUpdate*4)
-                .style('fill', function (d) { return colour(d.value); })
-            ;
-
-            return this;
-        };
+        // return instance:
         return this;
     }();
-
-    // call to update (only when retrieving data):
-    var fnUpdate = _.throttle(function(){
-        graph.updateGraph.call(graph);
-        twitChange = {children:[]};
-    }, iUpdate);
 
     // listen for data:
     var con = io.connect();
     con.on('data', function (data) {
-        iTwitCount++;
-        d3.select("#dvCount .count").text( format(iTwitCount) );
-
-        // find element and update, or insert:
-        var iName = data.len;
-        var o = _.find(twitData.children, function (itm) {
-            return itm.name == iName;
-        });
-        if (o)
-            o.value += 1;
-        else{
-            o = {name:iName, value:1};
-            twitData.children.push(o);
-        }
-
-        // is this a new change:
-        if (!_.find(twitChange.children, function (itm) { return itm.name == iName; }))
-            twitChange.children.push(o);
-
-        // got some data, trigger and update:
-        fnUpdate();
+        // got data, update our model:
+        fnUpdate(data.len);
     });
 
-})(io);
+};
+
+// helpful methods:
+var helpers = {
+    // get window size:
+    win: function(){
+        var db = document.body;
+        var dde = document.documentElement;
+
+        return {
+            width: Math.max(db.scrollWidth, dde.scrollWidth, db.offsetWidth, dde.offsetWidth, db.clientWidth, dde.clientWidth) - 30
+            , height: Math.max(db.scrollHeight, dde.scrollHeight, db.offsetHeight, dde.offsetHeight, db.clientHeight, dde.clientHeight) - 80
+        };
+    }
+}
